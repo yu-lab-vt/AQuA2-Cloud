3a8720e212bd1ddf41a68f99f153b7a36d736bce
bmark21 <106696695+bmark21@users.noreply.github.com>
2025-06-06 16:24:10 -0400

    Extend permitted inactivity time
    Allow users to idle for up to 30 minutes before automatically logging them out

--- aqua2_cloud_website/assets/setup/env.php
+++ aqua2_cloud_website/assets/setup/env.php
@@ -5,7 +5,7 @@ if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'CBIL
 if (!defined('APP_OWNER'))                      define('APP_OWNER', 'CBIL');
 if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'AQuA2 Cloud Processing Service');
 
-if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*300);
+if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*1800);
 
 if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'AQuA2_Cloud_Database');
 if (!defined('DB_HOST'))                        define('DB_HOST','127.0.0.1');
